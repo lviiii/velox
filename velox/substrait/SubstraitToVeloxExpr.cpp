f486b3a9d66a41aaf6437e3cb7af15e7eacbb5a7
lviiii <jiao.lv@intel.com>
2022-11-17 13:50:48 +0000

    Print registered function.

--- velox/substrait/SubstraitToVeloxExpr.cpp
+++ velox/substrait/SubstraitToVeloxExpr.cpp
@@ -18,6 +18,7 @@
 #include "velox/substrait/TypeUtils.h"
 #include "velox/substrait/VariantToVectorConverter.h"
 #include "velox/vector/VariantToVector.h"
+#include "iostream"
 
 using facebook::velox::core::variantArrayToVector;
 namespace facebook::velox::substrait {
@@ -118,6 +119,8 @@ SubstraitVeloxExprConverter::toVeloxExpr(
   const auto& veloxFunction =
       subParser_->findVeloxFunction(functionMap_, sFunc.function_reference());
   std::string typeName = subParser_->parseType(sFunc.output_type())->type;
+  std::cout << "Parsing Velox function: " << veloxFunction << 
+    ", output type: " << typeName << std::endl;
 
   if (veloxFunction == "extract") {
     return toExtractExpr(std::move(params), toVeloxType(typeName));
